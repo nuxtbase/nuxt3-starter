3d3ea9516e4416564a3255363d441b3e249b4920
Yunwei Xiao <xiaoluoboding@gmail.com>
2022-11-25 15:37:13 +0800

    feat: add favicon icon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,38 +1,39 @@
-import { defineNuxtConfig } from "nuxt/config"
-import ViteComponents from "unplugin-vue-components/vite"
-import IconsResolver from "unplugin-icons/resolver"
+import { defineNuxtConfig } from 'nuxt/config'
+import ViteComponents from 'unplugin-vue-components/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 
 export default defineNuxtConfig({
   app: {
     head: {
-      title: "A Better Nuxt 3 Starter",
-    },
+      title: 'A Better Nuxt 3 Starter',
+      link: [{ rel: 'icon', type: 'image/*', href: '/favicon.svg' }]
+    }
   },
   modules: [
-    "nuxt-windicss",
-    "@pinia/nuxt",
-    "unplugin-icons/nuxt",
-    "@vueuse/nuxt",
+    'nuxt-windicss',
+    '@pinia/nuxt',
+    'unplugin-icons/nuxt',
+    '@vueuse/nuxt',
     [
-      "@intlify/nuxt3",
+      '@intlify/nuxt3',
       {
-        localeDir: "locales",
+        localeDir: 'locales',
         vueI18n: {
-          locale: "en",
-        },
-      },
-    ],
+          locale: 'en'
+        }
+      }
+    ]
   ],
   vite: {
     plugins: [
       ViteComponents({
         resolvers: [
           IconsResolver({
-            componentPrefix: "",
-          }),
+            componentPrefix: ''
+          })
         ],
-        dts: true,
-      }),
-    ],
-  },
+        dts: true
+      })
+    ]
+  }
 })
